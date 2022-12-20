e5d9278c1e18f1c18b667e0c71c80a68a9433875
Porechnyi Denis <Pro_15.6_6@outlook.com>
2022-12-21 01:23:30 +0700

    Edit some html. Just a makeup

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,11 @@
+from django.conf import settings
 from django.shortcuts import get_object_or_404, render
-
-from yatube.settings import NUM_OF_POSTS
 
 from .models import Group, Post
 
 
 def index(request):
-    posts = Post.objects.all().select_related('author', 'group')[:NUM_OF_POSTS]
+    posts = Post.objects.select_related('author', 'group')[:settings.NUM_OF_POSTS]
     context = {
         'posts': posts,
     }
@@ -15,7 +14,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all().select_related('author', 'group')[:NUM_OF_POSTS]
+    posts = group.posts.select_related('author', 'group')[:settings.NUM_OF_POSTS]
     context = {
         'group': group,
         'posts': posts,
